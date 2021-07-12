456004a0b26260fa8334534c4b48353ae5278619
Eimantas <eimantas@riawolf.com>
2021-07-12 15:44:50 +0300

    - set up redux toolkit; - reorganized file structure;

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import "./App.css";
+
+const App: FC = (): JSX.Element => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome to my app</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/store/initialState.ts
@@ -0,0 +1,5 @@
+export interface IInitialState {}
+
+const initialState: IInitialState = {};
+
+export default initialState;
